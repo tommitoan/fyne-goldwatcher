b0b9878222395098ad67959bfacdf2e91bb95bb8
tommitoan <htm1995@gmail.com>
2024-05-02 21:18:19 +0700

    Init project

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
+
+type Config struct {
+	App      fyne.App
+	InfoLog  *log.Logger
+	ErrorLog *log.Logger
+	MainWindow fyne.Window
+}
+
+var myApp Config
+
+func main() {
+	// create a fyne application
+	fyneApp := app.NewWithID("ca.gocode.goldwatcher.preferences")
+	myApp.App = fyneApp
+
+	// create our loggers
+	myApp.InfoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	myApp.ErrorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// open a connection to the database
+
+	// create a database repository
+
+	// create and size a fyne window
+	myApp.MainWindow = fyneApp.NewWindow("GoldWatcher")
+	myApp.MainWindow.Resize(fyne.NewSize(300,200))
+	myApp.MainWindow.SetFixedSize(true)
+	myApp.MainWindow.SetMaster()
+
+	myApp.makeUI()
+
+	// show and run the application
+	myApp.MainWindow.ShowAndRun()
+}
